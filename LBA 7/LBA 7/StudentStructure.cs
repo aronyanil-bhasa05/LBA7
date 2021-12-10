91101aaf6a1f546ea61a1272e6e6ac6155f6e0c5
aronyanil-bhasa05 <bhasa05@gmail.com>
2021-12-10 17:15:52 +0530

    pushing first changes

---
+++ LBA 7/LBA 7/StudentStructure.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LBA_7
+{
+    struct Student
+    {
+        int roll;
+        string name;
+        string gender;
+        double mobile;
+        public Student(int roll, string name, string gender, double mobile)
+        {
+            this.roll = roll;
+            this.name = name;
+            this.gender = gender;
+            this.mobile = mobile;
+        }
+
+        public string Display()
+        {
+            return $"ROLL= {this.roll}\nNAME= {this.name}\nGENDER= {this.gender}\nMOBILE NO.= {this.mobile}";
+        }
+    }
+
+    class StudentStructure
+    {
+        static void Main ()
+        {
+            String c = "";
+            String sname, sgen;
+            Double smob;
+            int sroll;
+            Student stud = new Student();
+
+
+            while(c != "EXIT")
+            {
+                Console.Write("ADD/VIEW ENTER CHOICE = ");
+                c = Console.ReadLine();
+                if (c == "ADD")
+                {
+                    Console.Write("ENTER ROLL = ");
+                    sroll = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("ENTER NAME = ");
+                    sname = Console.ReadLine();
+                    Console.Write("ENTER GENDER = ");
+                    sgen = Console.ReadLine();
+                    Console.Write("ENTER MOBILE NUMBER = ");
+                    smob = Convert.ToDouble(Console.ReadLine());
+                    Student ob = new Student(sroll, sname, sgen, smob);
+                }
+                else if (c == "VIEW")
+                {
+                    Console.WriteLine(stud.Display());
+                }               
+            }
+            Console.ReadLine();
+        }
+        
+            
+    }
+}
